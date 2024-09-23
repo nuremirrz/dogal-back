187b0ab91803b3e001073c4b64db6d2fcc18cf09
Emirlan <nur.emirlan.02@gmail.com>
2024-09-23 13:10:57 +0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send('Backend is running');
+});
+
+app.get('/api/test', (req, res) => {
+    res.json({ message: 'API работает!' });
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
